24a32258f6e82572fb4a5d66f337f5e949e2e676
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-25 19:12:03 +0600

    Create Main.java

---
+++ problem-A/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MovableCircle a = new MovableCircle(1,2,3,4,5);
+        System.out.println(a);
+    }
+}
