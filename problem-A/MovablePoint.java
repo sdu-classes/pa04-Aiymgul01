6eef04fe27e426cfc39e1bc1aa6f9f0a2edcad99
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-25 19:17:22 +0600

    Update MovablePoint.java

--- problem-A/MovablePoint.java
+++ problem-A/MovablePoint.java
@@ -1,37 +1,38 @@
-package com.company;
-
-public class MoveablePoint implements Movaable {
+public class MovablePoint implements Movable {
     int x;
     int y;
     int xSpeed;
     int ySpeed;
-    public MoveablePoint(int x, int y, int xSpeed, int ySpeed){
-        this.x=x;
-        this.y=y;
-        this.xSpeed=xSpeed;
-        this.ySpeed=ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
 
-    public String toString(){
-        return "("+x+", "+y+"), speed = ("+xSpeed+", "+ySpeed+")";
+    public String toString() {
+        return String.format("(%s,%s), speed = (%s,%s)", x, y, this.xSpeed, this.ySpeed);
     }
 
     @Override
     public void moveUp() {
-        y+=ySpeed;
+        this.y -= this.ySpeed;
     }
+
     @Override
     public void moveDown() {
-        y-=ySpeed;
+        this.y += this.ySpeed;
     }
+
     @Override
     public void moveLeft() {
-        x-=xSpeed;
+        this.x -= this.xSpeed;
     }
 
     @Override
     public void moveRight() {
-        x+=xSpeed;
-
+        this.x += this.xSpeed;
     }
+
 }
