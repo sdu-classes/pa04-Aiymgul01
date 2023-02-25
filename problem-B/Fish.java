d76e13a7fa3f9ef828d5002457f9ee1428578a24
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-26 01:52:01 +0600

    Update Fish.java

--- problem-B/Fish.java
+++ problem-B/Fish.java
@@ -1,32 +1,23 @@
-public class Fish extends Animal implements Pet{
-    String name;
-
-    public Fish(String name){
+class Fish extends Animal implements Pet{
+    private String name;
+    public Fish() {
         super(0);
-        this.name = name;
     }
 
-    public Fish(){
-        this("Nemo");
+    public void walk() {
+        System.out.println("Fish can't walk");
     }
-    @Override
-    public String getName(){
-        return name;
+
+    public void eat() {
+        System.out.println("The fish eats");
     }
-    @Override
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
-    @Override
     public void play(){
-        System.out.println("Fishes are playing");
+        System.out.println("Fish can't play with you.");
     }
-    @Override 
-    public void walk(){
-        System.out.println("Fishes they do not have legs and can not walk :)");
-    }
-    @Override
-    public void eat(){
-        System.out.println("Fishes are eating water insects");
+    public String getName() {
+        return name;
     }
 }
