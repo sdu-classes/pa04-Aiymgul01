49d595d80515a744e7bb8d4e149ec7a6b49e69fb
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-25 19:12:49 +0600

    Delete Main.java

--- problem-A/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	MovableCircle a = new MovableCircle(1,2,3,4,5);
-        System.out.println(a);
-    }
-}
